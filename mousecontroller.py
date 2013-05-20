7134d57c387506692e2ca57483aeefb80f123630
awatekiran <awatekiran@outlook.com>
2013-05-20 10:22:18 +0530

    Create mousecontroller.py

---
+++ mousecontroller.py
@@ -0,0 +1,30 @@
+import simplegui
+import math
+
+width = 500
+height = 400
+ball_pos = [width/2, height/2]
+ball_radius = 20
+ball_colour = "Red"
+
+def distance(p, q):
+    return math.sqrt( (p[0]-q[0])**2 + (p[1]-p[1])**2)
+
+def click(pos):
+    global ball_pos, ball_colour
+    
+    if distance(pos, ball_pos) < ball_radius:
+        ball_colour = "Blue"
+    else:
+        ball_pos = list(pos)
+        ball_colour = "Red"
+
+def draw(canvas):
+    canvas.draw_circle(ball_pos, ball_radius, 1, ball_colour, ball_colour)
+
+frame = simplegui.create_frame("Test", width, height)
+frame.set_canvas_background("White")
+frame.set_draw_handler(draw)
+frame.set_mouseclick_handler(click)
+
+frame.start()
